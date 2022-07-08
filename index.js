612cdd3f63055eae098ffbd0452ed51328ec1769
Yentzer Sensess <sensessyentzer@outlook.com>
2022-07-07 20:38:55 -0400

    File Setup + Break Down Code into Small Chunks

---
+++ index.js
@@ -0,0 +1,16 @@
+const inquirer = require('inquirer')
+const template = require('./src/page-template')
+const writeFile = require('./src/write-file')
+
+//Constructor Classes + Questions for Employees
+
+
+//Store Team Member Objects
+
+//Manager Questions -> Create Object from Input
+
+//Engineer Questions -> Create Object from Input
+
+//Intern Questions -> Create Object from Input
+
+// Prompt Handling
---
+++ src/page-template.js
@@ -0,0 +1,11 @@
+// Manager Template
+
+// Engineer Template
+
+// Intern Template
+
+
+//Iterate Answers -> Create card for each member
+
+
+//HTML Page Template
